677e4ed4c223b9703aff17374743da6dc3e2e236
Jesus Checa Chaves <juchebo1@hotmail.com>
2017-11-24 11:16:41 +0100

    primer commit

---
+++ calcfact.c
@@ -0,0 +1,21 @@
+emiferale
+//commmmentario
+
+ dancaschi
+
+// Función que calcula el factorial de un número num
+// y  ademas...
+//comentario de rafromsan
+
+
+emiferale
+master
+
+ master
+master
+>>>>>>> 74db2b28d188779de020401c2761d5f9c1546321
+int calcfact(int num)
+{
+  if (num <= 1) return 1;
+  else return (num*calcfact(num-1));
+}
---
+++ hello.c
@@ -0,0 +1,21 @@
+emiferale
+#include <stdio.h>
+//comentario
+
+///otro comentario inutil
+#include <stdio.h>
+
+master
+int main()
+{
+  printf("Hola mundo\n");
+  printf("Adios mundo cruel\n");
+  printf("Hola PGPI\n\n\n");
+ 	printf("alvmargar4");
+  printf("hello world\n");
+}
+
+
+/* Modificación alvmargar4 */
+//modificado a posteriori por juamonzam
+
